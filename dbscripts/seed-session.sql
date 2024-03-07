4036701e77a2b991ccae9928bd4b2bf8e2328209
Anthony Paboeuf <delsustheugm@gmail.com>
2024-03-07 16:53:32 +0100

    adding session

---
+++ dbscripts/seed-session.sql
@@ -0,0 +1,23 @@
+﻿\connect session
+
+-- Table: public.session
+
+-- DROP TABLE IF EXISTS public.session;
+
+CREATE TABLE IF NOT EXISTS public.session
+(
+    "Id" text COLLATE pg_catalog."default" NOT NULL,
+    "StartedAt" bigint NOT NULL,
+    "ClosedAt" bigint,
+    "LocationQrCode" text COLLATE pg_catalog."default" NOT NULL,
+    "Orders" text[] COLLATE pg_catalog."default" NOT NULL,
+    "TotalPrice" real NOT NULL,
+    "CurrentlyPaid" real NOT NULL,
+    "ActuallyProcessingPayment" boolean NOT NULL,
+    CONSTRAINT "PK_session" PRIMARY KEY ("Id")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.session
+    OWNER to admin_user;
